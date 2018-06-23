6c9efdbcf3b77c178fe625a989108896e6859e4f
Hayden Betts <haydenbetts@gmail.com>
2018-06-23 18:18:16 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 def line(line_array)
   if line_array.length == 0
-    return "The line is currently empty."
+    puts "The line is currently empty."
   else
     concat = line_array.collect.with_index do |patron, index|
       " #{index}. #{patron}"
