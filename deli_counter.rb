df05070504947eca785081d8c448af343d3fdbc8
Hayden Betts <haydenbetts@gmail.com>
2018-06-23 18:18:01 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,9 @@
 # Write your code here.
-def line(line)
-  if line.length == 0
+def line(line_array)
+  if line_array.length == 0
     return "The line is currently empty."
   else
-    concat = line.collect.with_index do |patron, index|
+    concat = line_array.collect.with_index do |patron, index|
       " #{index}. #{patron}"
     end
     return "The line is current:#{concat}"
