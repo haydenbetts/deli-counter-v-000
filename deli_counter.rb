afad65649cc41c9bc875a8abe87d8874578f90a5
Hayden Betts <haydenbetts@gmail.com>
2018-06-23 18:14:08 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+def line(line)
+  if line.length == 0
+    return "The line is currently empty."
+  else
+    line.collect.with_index do |patron, index|
+      
+    end
+
+
+end
