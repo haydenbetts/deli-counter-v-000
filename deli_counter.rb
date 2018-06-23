1921c5b49c6f724e4caab37ee68f2f7df16c2991
Hayden Betts <haydenbetts@gmail.com>
2018-06-23 18:29:50 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,6 +19,6 @@ def now_serving(line_array)
   if line_array.length == 0
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{line_array.pop}."
+    puts "Currently serving #{line_array.first}."
   end
 end
